5e57d99d77cdc51956dda583a2b7006caf7a4f10
Nikan Radan <github.com@alias.nikanradan.com>
2022-08-13 18:31:48 -0700

    1.19.2

--- Human Reborn/build.gradle.kts
+++ Human Reborn/build.gradle.kts
@@ -11,19 +11,18 @@ val modVersion: String by project
 version = modVersion
 val mavenGroup: String by project
 group = mavenGroup
-minecraft {}
 repositories {}
 dependencies {
     val minecraftVersion: String by project
-    minecraft("com.mojang:minecraft:$minecraftVersion")
+    minecraft("com.mojang", "minecraft", minecraftVersion)
     val yarnMappings: String by project
-    mappings("net.fabricmc:yarn:$yarnMappings:v2")
+    mappings("net.fabricmc", "yarn", yarnMappings, null, "v2")
     val loaderVersion: String by project
-    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+    modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
     val fabricVersion: String by project
-    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
+    modImplementation("net.fabricmc.fabric-api", "fabric-api", fabricVersion)
     val fabricKotlinVersion: String by project
-    modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
+    modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
 }
 tasks {
     val javaVersion = JavaVersion.VERSION_17
@@ -33,11 +32,7 @@ tasks {
         targetCompatibility = javaVersion.toString()
         options.release.set(javaVersion.toString().toInt())
     }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions { jvmTarget = javaVersion.toString() }
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> { kotlinOptions { jvmTarget = javaVersion.toString() } }
     jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
     processResources {
         inputs.property("version", project.version)

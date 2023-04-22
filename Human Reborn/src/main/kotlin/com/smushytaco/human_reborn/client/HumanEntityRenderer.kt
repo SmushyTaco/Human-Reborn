7e8551aeaf9451f8e9dd0c9c5b575b68c6fbf105
Nikan Radan <github.com@alias.nikanradan.com>
2023-04-21 22:55:49 -0700

    1.19.4

--- Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/client/HumanEntityRenderer.kt
+++ Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/client/HumanEntityRenderer.kt
@@ -12,8 +12,7 @@ import net.minecraft.util.Identifier
 @Environment(EnvType.CLIENT)
 class HumanEntityRenderer(ctx: EntityRendererFactory.Context): BipedEntityRenderer<MobEntity, PlayerEntityModel<MobEntity>>(ctx, PlayerEntityModel(ctx.getPart(EntityModelLayers.PLAYER), false), 0.5F) {
     init {
-        addFeature(ArmorFeatureRenderer(this, BipedEntityModel(ctx.getPart(EntityModelLayers.PLAYER_INNER_ARMOR)), BipedEntityModel(ctx.getPart(EntityModelLayers.PLAYER_OUTER_ARMOR)))
-        )
+        addFeature(ArmorFeatureRenderer(this, BipedEntityModel(ctx.getPart(EntityModelLayers.PLAYER_INNER_ARMOR)), BipedEntityModel(ctx.getPart(EntityModelLayers.PLAYER_OUTER_ARMOR)), ctx.modelManager))
     }
     override fun getTexture(entity: MobEntity): Identifier = Identifier("minecraft", "textures/entity/player/wide/steve.png")
 }

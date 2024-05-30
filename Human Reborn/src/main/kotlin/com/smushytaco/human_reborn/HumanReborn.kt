03d8f1da59d806acfac80b1511dc71a5984c94c1
Nikan Radan <github.com@alias.nikanradan.com>
2024-05-30 13:26:41 -0700

    1.20.6

--- Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/HumanReborn.kt
+++ Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/HumanReborn.kt
@@ -1,15 +1,12 @@
 package com.smushytaco.human_reborn
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricDefaultAttributeRegistry
-import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
-import net.minecraft.entity.EntityDimensions
-import net.minecraft.entity.EntityType
-import net.minecraft.entity.SpawnGroup
-import net.minecraft.entity.SpawnRestriction
+import net.minecraft.entity.*
 import net.minecraft.entity.mob.HostileEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroups
 import net.minecraft.item.SpawnEggItem
@@ -20,13 +17,12 @@ import net.minecraft.world.Heightmap
 object HumanReborn: ModInitializer {
     private const val MOD_ID = "human_reborn"
     val HUMAN_ENTITY_TYPE: EntityType<HumanEntity> = Registry.register(Registries.ENTITY_TYPE, Identifier(MOD_ID, "human"),
-        FabricEntityTypeBuilder.create(SpawnGroup.MONSTER) { _, world ->
-            HumanEntity(world)
-        }.dimensions(EntityDimensions.fixed(0.6F, 1.8F)).trackRangeChunks(8).build())
+        EntityType.Builder.create({ _, world -> HumanEntity(world) }, SpawnGroup.MONSTER).dimensions(0.6F, 1.8F)
+            .eyeHeight(1.62F).vehicleAttachment(PlayerEntity.VEHICLE_ATTACHMENT_POS).maxTrackingRange(8).build())
     private val HUMAN_SPAWN_EGG = SpawnEggItem(HUMAN_ENTITY_TYPE, 5651507, 12422002, Item.Settings())
     override fun onInitialize() {
         FabricDefaultAttributeRegistry.register(HUMAN_ENTITY_TYPE, HumanEntity.createMobAttributes())
-        SpawnRestriction.register(HUMAN_ENTITY_TYPE, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark)
+        SpawnRestriction.register(HUMAN_ENTITY_TYPE, SpawnLocationTypes.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, HostileEntity::canSpawnInDark)
         BiomeModifications.addSpawn(BiomeSelectors.spawnsOneOf(EntityType.ZOMBIE), SpawnGroup.MONSTER, HUMAN_ENTITY_TYPE, 10, 1, 3)
         Registry.register(Registries.ITEM, Identifier(MOD_ID, "human_spawn_egg"), HUMAN_SPAWN_EGG)
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(ItemGroupEvents.ModifyEntries { it.add(HUMAN_SPAWN_EGG) })

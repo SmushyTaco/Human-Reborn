9d726fd4069bd00ebe6780b58ae52f14bbe35f0f
Nikan Radan <personal@nikanradan.com>
2024-12-12 19:52:45 -0800

    1.21.4

--- Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/client/HumanEntityRenderer.kt
+++ Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/client/HumanEntityRenderer.kt
@@ -1,8 +1,6 @@
 package com.smushytaco.human_reborn.client
 import com.smushytaco.human_reborn.HumanEntity
 import com.smushytaco.human_reborn.HumanReborn
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.minecraft.client.render.entity.EntityRendererFactory
 import net.minecraft.client.render.entity.LivingEntityRenderer
 import net.minecraft.client.render.entity.feature.*
@@ -11,16 +9,15 @@ import net.minecraft.client.render.entity.model.EntityModelLayers
 import net.minecraft.client.render.entity.model.PlayerEntityModel
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState
 import net.minecraft.util.Identifier
-@Environment(EnvType.CLIENT)
 class HumanEntityRenderer(ctx: EntityRendererFactory.Context): LivingEntityRenderer<HumanEntity, PlayerEntityRenderState, PlayerEntityModel>(ctx, PlayerEntityModel(ctx.getPart(EntityModelLayers.PLAYER), false), 0.5F) {
     init {
         addFeature(ArmorFeatureRenderer(this, ArmorEntityModel(ctx.getPart(EntityModelLayers.PLAYER_INNER_ARMOR)), ArmorEntityModel(ctx.getPart(EntityModelLayers.PLAYER_OUTER_ARMOR)), ctx.equipmentRenderer))
-        addFeature(PlayerHeldItemFeatureRenderer(this, ctx.itemRenderer))
+        addFeature(PlayerHeldItemFeatureRenderer(this))
         addFeature(StuckArrowsFeatureRenderer(this, ctx))
-        addFeature(HeadFeatureRenderer(this, ctx.modelLoader, ctx.itemRenderer))
-        addFeature(ElytraFeatureRenderer(this, ctx.modelLoader, ctx.equipmentRenderer))
-        addFeature(ShoulderParrotFeatureRenderer(this, ctx.modelLoader))
-        addFeature(TridentRiptideFeatureRenderer(this, ctx.modelLoader))
+        addFeature(HeadFeatureRenderer(this, ctx.entityModels))
+        addFeature(ElytraFeatureRenderer(this, ctx.entityModels, ctx.equipmentRenderer))
+        addFeature(ShoulderParrotFeatureRenderer(this, ctx.entityModels))
+        addFeature(TridentRiptideFeatureRenderer(this, ctx.entityModels))
         addFeature(StuckStingersFeatureRenderer(this, ctx))
     }
     override fun getTexture(state: PlayerEntityRenderState): Identifier = Identifier.of("minecraft", "textures/entity/player/wide/steve.png")
--- Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/client/HumanRebornClient.kt
+++ Human Reborn/src/main/kotlin/com/smushytaco/human_reborn/client/HumanRebornClient.kt
@@ -1,9 +1,6 @@
 package com.smushytaco.human_reborn.client
 import com.smushytaco.human_reborn.HumanReborn
 import net.fabricmc.api.ClientModInitializer
-import net.fabricmc.api.EnvType
-import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry
 @Suppress("UNUSED")
-@Environment(EnvType.CLIENT)
 object HumanRebornClient: ClientModInitializer { override fun onInitializeClient() { EntityRendererRegistry.register(HumanReborn.HUMAN_ENTITY_TYPE) { HumanEntityRenderer(it) } } }
